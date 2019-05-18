dc52051205a58fc82bc711434ae08531715b9f91
Więcław Krzysztof <wutnik@gmail.com>
2019-05-18 19:56:49 +0200

    Fixed time and number of colors issues

--- Chromatic/Chromatic/ConnectedSeqeuentialColoring.cpp
+++ Chromatic/Chromatic/ConnectedSeqeuentialColoring.cpp
@@ -1,6 +1,7 @@
 #include <boost/graph/breadth_first_search.hpp>
 #include "ConnectedSeqeuentialColoring.h"
 #include <boost/timer.hpp>
+#include <chrono>
 
 std::string ConnectedSeqeuentialColoring::_name = "Connected Sequential";
 std::string ConnectedSeqeuentialColoring::_description = "Colors graph with BFS alghoritm";
@@ -17,25 +18,24 @@ ConnectedSeqeuentialColoring::~ConnectedSeqeuentialColoring()
 struct connected_sequential_visitor : public boost::default_bfs_visitor
 {
 public:
-	size_t _number_of_colors;
 
-	connected_sequential_visitor(std::vector<size_t> & colors) : _colors(colors)
+	connected_sequential_visitor(std::vector<size_t>& colors, size_t& number_of_colors) : _colors(colors), _number_of_colors(number_of_colors)
 	{
-		_number_of_colors = 0;
+		_number_of_colors.get() = 0;
 	}
 	
 	void examine_vertex(const Graph::vertex_descriptor &s, const Graph &g)
 	{
-		if (_number_of_colors == 0)
+		if (_number_of_colors.get() == 0)
 		{
 			_colors.get()[s] = 0;
-			_number_of_colors = 1;
+			_number_of_colors.get() = 1;
 			return;
 		}
 
 		auto neighbours = boost::adjacent_vertices(s, g);
-		std::vector<bool> taken_colors(_number_of_colors, false);
-		size_t colors_left = _number_of_colors;
+		std::vector<bool> taken_colors(_number_of_colors.get(), false);
+		size_t colors_left = _number_of_colors.get();
 		for (auto neighbour : make_iterator_range(neighbours))
 		{
 			size_t n_color = _colors.get()[neighbour];
@@ -46,15 +46,15 @@ public:
 				colors_left--;
 				if (colors_left == 0)
 				{
-					_colors.get()[s] = _number_of_colors;
-					_number_of_colors += 1;
+					_colors.get()[s] = _number_of_colors.get();
+					_number_of_colors.get() += 1;
 					return;
 				}
 				taken_colors[n_color] = true;
 			}
 		}
 
-		for (int i = 0; i < _number_of_colors; ++i)
+		for (int i = 0; i < _number_of_colors.get(); ++i)
 		{
 			if (taken_colors[i] == false)
 			{
@@ -65,18 +65,19 @@ public:
 	}
 
 	std::reference_wrapper<std::vector<size_t>> _colors;
+	std::reference_wrapper<size_t> _number_of_colors;
 };
 
 void ConnectedSeqeuentialColoring::Run()
 {
-	boost::timer timer;
-	timer.restart();
-	connected_sequential_visitor vis(_colors);
+	auto start = std::chrono::system_clock::now();
+	connected_sequential_visitor vis(_colors, _number_of_colors);
 	
 	boost::breadth_first_search(_graph, boost::vertex(0, _graph), visitor(vis));
 	_number_of_colors = vis._number_of_colors;
 
-	_time = timer.elapsed();
+	auto end = std::chrono::system_clock::now();
+	_time = std::chrono::duration<double>(end - start).count();
 }
 
 const Graph & ConnectedSeqeuentialColoring::ColoredGraph()
